afdf9734b32a23402d1b2b2bbcb0b284652d5551
Frank van Wijk <fvanwijk@xebia.com>
2016-03-15 21:49:20 +0100

    Mox config is now done on Mox (push) instead of from Mox (pull)

--- test/moxConfig.js
+++ test/moxConfig.js
@@ -1,7 +1,9 @@
-angular.extend(moxConfig, {
+var m = mox.helpers;
+
+mox.factories = {
   $routeParams: function ($provide, params) {
     var mock = angular.extend({}, params || {});
     mox.save($provide, '$routeParams', mock);
     return mock;
   }
-});
+};
